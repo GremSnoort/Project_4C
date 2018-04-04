4274adbde790839f3ceffb6aeedb26fe364b3d78
unknown <grem_snoort@protonmail.com>
2018-04-04 23:30:20 +0900

    NO DELETIONS

--- app/src/main/java/com/example/eva/project_4c/SplashActivity.kt
+++ app/src/main/java/com/example/eva/project_4c/SplashActivity.kt
@@ -8,10 +8,14 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import kotlin.concurrent.thread
 import android.webkit.WebView
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.*
 
 
 class SplashActivity : Activity() {
 
+    fun ClosedRange<Int>.random() = Random().nextInt(endInclusive + 1 - start) + start
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -22,8 +26,22 @@ class SplashActivity : Activity() {
         mWebView.setBackgroundColor(Color.TRANSPARENT)
         mWebView.setPadding(0, 0, 0, 0)
 
-        val anim = object : Animation()
-        anim = AnimationUtils.loadAnimation(this,
+        val rand_choice = (0..4).random()
+        var Id: Int = R.anim.myalpha
+        if (rand_choice==0)
+            Id = R.anim.myalpha
+        else if (rand_choice==1)
+            Id = R.anim.mycombo
+        else if (rand_choice==2)
+            Id = R.anim.myrotate
+        else if (rand_choice==3)
+            Id = R.anim.myscale
+        else if (rand_choice==4)
+            Id = R.anim.mytrans
+
+        val anm =  AnimationUtils.loadAnimation(this, Id)
+        val mPic = findViewById<ImageView>(R.id.droid_pic)
+        mPic.startAnimation(anm)
 
 
         val myThread = object : Thread() {
