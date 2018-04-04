d5ec8114c08d264b9e0b34595d3f2a9ff4423102
unknown <grem_snoort@protonmail.com>
2018-04-04 14:32:51 +0900

    splash screen 2 var

--- app/src/main/java/com/example/eva/project_4c/SplashActivity.kt
+++ app/src/main/java/com/example/eva/project_4c/SplashActivity.kt
@@ -2,8 +2,10 @@ package com.example.eva.project_4c
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import kotlin.concurrent.thread
+import android.webkit.WebView
 
 
 class SplashActivity : Activity() {
@@ -11,6 +13,16 @@ class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val mWebView = findViewById<WebView>(R.id.webview)
+        //mWebView.loadUrl("https://i.gifer.com/WG94.gif")
+
+        mWebView.setBackgroundColor(Color.TRANSPARENT)
+        mWebView.setPadding(0, 0, 0, 0)
+
+        mWebView.loadUrl("http://netsources.narod.ru/friday/alkocat.jpg");
+
+
 
         val myThread = object : Thread() {
             override fun run() {
